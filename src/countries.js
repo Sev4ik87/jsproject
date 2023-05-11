52f718a86ebbf81ce8d521195267ce9ce921d227
Sev4ik87 <se8v7a@gmail.com>
2023-05-11 22:01:01 +0300

    simplerating

--- src/countries.js
+++ src/countries.js
@@ -4,27 +4,27 @@ function getObj() {
 }
 
 
-function Country (title, capital, population, area) {
+function Country(title, capital, population, area) {
 	this.title = title;
 	this.capital = capital;
 	this.population = population;
 	this.area = area;
-	this.getCountry = function (name) {
-    for (let item in name) 
-{
-  if(typeof name[item] === 'string'){
-    console.log(this)};
-} 
+	this.getCountry = getObj ;
+ 
   }
-}
+
 const Germany = new Country('Germany', 'Berlin', '84.3 million', '357588km');
 const France = new Country('France', 'Paris', '67.75 million', '551695km');
 
-function choose(name) {
-name.getCountry(name);
+console.log(Germany);
+console.log(France);
 
+function showItem(name) {
+for (let item in name){
+  if ( typeof name[item] !== 'function') {
+console.log(`${item}: ${name.getCountry()[item]}`);
 }
-choose(Germany);
- choose(France);
-// Germany.getCountry();
-// France.getCountry();
+}
+}
+showItem(Germany);
+showItem(France);
--- src/ratingsimple.js
+++ src/ratingsimple.js
@@ -3,42 +3,33 @@
  *
  * @param {number} rate1 first operator
  * @param {number} rate2 second operator
- * @param {number} result additional container
  */
 function setRate(rate1, rate2) {
-  let result = 0;
+  let diffrate = rate1 - rate2;
   if (
-    !Number.isNaN(rate1) &&
-    !Number.isNaN(rate2) &&
-    typeof rate1 == "number" &&
-    typeof rate2 == "number"
+    Number.isNaN(diffrate) ||
+    typeof rate1 !== "number" ||
+    typeof rate2 !== "number" ||
+    rate1 < 0 ||
+    rate2 < 0
   ) {
-    if (rate1 >= 0 && rate2 >= 0) {
-      if (rate1 === 0) {
-        return (rate1 += rate2);
-      }
-      if (rate1 >= rate2) {
-        if (rate1 - rate2 >= 0 && rate1 - rate2 <= 2) {
-          return (rate1 += 2);
-        }
-        if (rate1 - rate2 > 2 && rate1 - rate2 < 20) {
-          return (rate1 += 1);
-        }
-        if (rate1 - rate2 >= 20) {
-          return rate1;
-        }
-      } else {
-        if (rate1 < rate2) {
-          result += Math.round((rate2 - rate1 + 5) / 30);
-          rate1 += result;
-          return rate1;
-        }
-      }
-    } else {
-      console.log("Wrong rate1 or rate2");
-    }
-  } else {
-    console.log("Plese use number type");
+    return "Entered wrong rate";
+  }
+  if (rate1 === 0) {
+    return rate2;
+  }
+  if (diffrate >= 0 && diffrate <= 2) {
+    return (rate1 += 2);
+  }
+
+  if (diffrate > 2 && diffrate < 20) {
+    return (rate1 += 1);
+  }
+  if (diffrate >= 20) {
+    return rate1;
+  }
+  if (diffrate < 0) {
+    return rate1 + Math.round((Math.abs(diffrate) + 5) / 3);
   }
 }
 
