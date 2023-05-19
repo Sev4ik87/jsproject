8e098370fbb48c101314d8e7ee59756d9aba2807
Sev4ik87 <se8v7a@gmail.com>
2023-05-19 14:06:05 +0300

    add recursion

--- src/getnumber.js
+++ src/getnumber.js
@@ -1,34 +1,60 @@
-'use strict';
 /**
- * @param {number} targetNumber - Заданное число
- * @returns {string} -  сообщение об невозможности.
+ *
+ * @param {number} targetNumber - Заданное число.
+ * @returns {string} - Строка, описывающая получение заданного числа, или сообщение об невозможности.
  */
-function getNumber(targetNumber) {
+function describeNumber(targetNumber) {
   if (targetNumber === 1) {
-    return "1";
+    return '1';
   }
-
-  if (targetNumber % 3 === 0) {
-    const expression = getNumber(targetNumber / 3);
-    if (expression !== "Impossible") {
-      return `(${expression} * 3)`;
+  
+  function findExpression(currentNumber, expr) {
+    if (currentNumber === targetNumber) {
+      return expr;
     }
-  }
-
-  if (targetNumber >= 5) {
-    const expression = getNumber(targetNumber - 5);
-    if (expression !== "Impossible") {
-      return `(${expression} + 5)`;
+    
+    if (currentNumber > targetNumber) {
+      return null;
     }
+    
+    const expr1 = findExpression(currentNumber * 3, `(${expr} * 3)`);
+    if (expr1) {
+      return expr1;
+    }
+    
+    const expr2 = findExpression(currentNumber * 5, `(${expr} * 5)`);
+    if (expr2) {
+      return expr2;
+    }
+    
+    const expr3 = findExpression(currentNumber + 3, `(${expr} + 3)`);
+    if (expr3) {
+      return expr3;
+    }
+    
+    const expr4 = findExpression(currentNumber + 5, `(${expr} + 5)`);
+    if (expr4) {
+      return expr4;
+    }
+    
+    return null;
   }
-
-  return "Impossible";
+  
+  const result = findExpression(1, "1");
+  if (result) {
+    return result;
+  }
+  
+  return 'Невозможно получить число';
 }
 
 
-console.log(getNumber(13)); 
-console.log(getNumber(22)); 
-console.log(getNumber(23)); 
+
+console.log(describeNumber(9));
+console.log(describeNumber(10));
+console.log(describeNumber(13));
+console.log(describeNumber(23));
+console.log(describeNumber(33));
 
 
 
