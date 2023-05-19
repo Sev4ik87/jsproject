9b872477f3173937b8684df452b27e18a59a10a8
Sev4ik87 <se8v7a@gmail.com>
2023-05-19 14:48:46 +0300

    add parametr

--- src/getnumber.js
+++ src/getnumber.js
@@ -1,17 +1,17 @@
+'use strict';
+
 /**
- * Функция для получения строки, описывающей, как получить заданное число с помощью математических операций над числом "1".
- * Разрешены только операции умножения и сложения с коэффициентами "3" и "5".
- * Искомое число передается в аргументы функции.
- *
  * @param {number} targetNumber - Заданное число.
- * @returns {string} - Строка, описывающая получение заданного числа в формате "число=выражение", или сообщение об невозможности.
+ * @returns {string|null} - Строка, описывающая получение заданного числа или сообщение об невозможности.
+ *  @param {number} currentNumber - Текущее число.
+ * @param {string} expr - Строка, содержащая текущую комбинацию операций.
  */
 function describeNumber(targetNumber) {
   if (targetNumber === 1) {
     return '1=1';
   }
   
-  function findExpression(currentNumber, expr) {
+  function getNumber(currentNumber, expr) {
     if (currentNumber === targetNumber) {
       return `${targetNumber}=${expr}`;
     }
@@ -20,22 +20,22 @@ function describeNumber(targetNumber) {
       return null;
     }
     
-    const expr1 = findExpression(currentNumber * 3, `(${expr} * 3)`);
+    const expr1 = getNumber(currentNumber * 3, `(${expr} * 3)`);
     if (expr1) {
       return expr1;
     }
     
-    const expr2 = findExpression(currentNumber * 5, `(${expr} * 5)`);
+    const expr2 = getNumber(currentNumber * 5, `(${expr} * 5)`);
     if (expr2) {
       return expr2;
     }
     
-    const expr3 = findExpression(currentNumber + 3, `(${expr} + 3)`);
+    const expr3 = getNumber(currentNumber + 3, `(${expr} + 3)`);
     if (expr3) {
       return expr3;
     }
     
-    const expr4 = findExpression(currentNumber + 5, `(${expr} + 5)`);
+    const expr4 = getNumber(currentNumber + 5, `(${expr} + 5)`);
     if (expr4) {
       return expr4;
     }
@@ -43,7 +43,7 @@ function describeNumber(targetNumber) {
     return null;
   }
   
-  const result = findExpression(1, "1");
+  const result = getNumber(1, "1");
   if (result) {
     return result;
   }
@@ -51,7 +51,7 @@ function describeNumber(targetNumber) {
   return 'Невозможно получить число';
 }
 
-
+console.log(describeNumber(1));
 console.log(describeNumber(2));
 console.log(describeNumber(9));
 console.log(describeNumber(10));
