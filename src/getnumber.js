4809d452e49d10bc26fc838d9c637e1099db7ef6
Sev4ik87 <se8v7a@gmail.com>
2023-05-19 16:48:20 +0300

    add JSDoc

--- src/getnumber.js
+++ src/getnumber.js
@@ -1,54 +1,59 @@
-'use strict';
+"use strict";
 
 /**
  * @param {number} targetNumber - Заданное число.
  * @returns {string|null} - Строка, описывающая получение заданного числа или сообщение об невозможности.
- *  @param {number} currentNumber - Текущее число.
+ * @param {number} currentNumber - Текущее число.
  * @param {string} expr - Строка, содержащая текущую комбинацию операций.
+ * @param {string} expr1 - Строка, содержащая текущую комбинацию операций.
+ * @param {string} expr2 - Строка, содержащая текущую комбинацию операций.
+ * @param {string} expr3 - Строка, содержащая текущую комбинацию операций.
+ * @param {string} expr4 - Строка, содержащая текущую комбинацию операций.
  */
+
 function describeNumber(targetNumber) {
   if (targetNumber === 1) {
-    return '1=1';
+    return "1=1";
   }
-  
+
   function getNumber(currentNumber, expr) {
     if (currentNumber === targetNumber) {
       return `${targetNumber}=${expr}`;
     }
-    
+
     if (currentNumber > targetNumber) {
       return null;
     }
-    
+
     const expr1 = getNumber(currentNumber * 3, `(${expr} * 3)`);
     if (expr1) {
       return expr1;
     }
-    
+
     const expr2 = getNumber(currentNumber * 5, `(${expr} * 5)`);
     if (expr2) {
       return expr2;
     }
-    
+
     const expr3 = getNumber(currentNumber + 3, `(${expr} + 3)`);
     if (expr3) {
       return expr3;
     }
-    
+
     const expr4 = getNumber(currentNumber + 5, `(${expr} + 5)`);
     if (expr4) {
       return expr4;
     }
-    
+
     return null;
   }
-  
+
   const result = getNumber(1, "1");
   if (result) {
     return result;
   }
-  
-  return 'Невозможно получить число';
+
+  return "Невозможно получить число";
 }
 
 console.log(describeNumber(1));
@@ -58,7 +63,3 @@ console.log(describeNumber(10));
 console.log(describeNumber(13));
 console.log(describeNumber(23));
 console.log(describeNumber(33));
-
-
-
-
