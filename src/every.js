cf8dc82f9d5ba02384c38ba396a909bd141d816a
Sev4ik87 <se8v7a@gmail.com>
2023-05-24 21:18:07 +0300

    add prototype

--- src/every.js
+++ src/every.js
@@ -1,20 +1,20 @@
 "use strict";
-
+function Myevery(){
 this.every = function(fn) {
   if (this.length === 0) {
     return false;
   }
   for (let i = this.length - 1; i >= 0; i--) {
-      if (this[i] === 0) {
+      if (this[el] === 0) {
         return false;
       }
       if (!fn(this[i], i, this)) {
         return false;
       }
-    
+      return false;
   }
-  return true;
+}
 }
 
-const myArr = [0, 10, 20];
-console.log(myArr.every(el => el % 2 === 0)); 
+const myArr = new Array (0, 10, 20);
+console.log(myArr.every (el => el % 2 === 0)); 
--- src/reverse.js
+++ src/reverse.js
@@ -1,12 +1,13 @@
 "use strict";
 
+function Myreverse(){
 
 this.reverse = function() {
   for (let i = 0, j = this.length - 1; i < j; i++, j--) {
     [this[i], this[j]] = [this[j], this[i]];
   }
   return this;
-};
-
-const myArray = [1, 2, 3, 4, 5];
+}
+}
+const myArray = new Array (1, 2, 3, 4, 5);
 console.log(myArray.reverse());
